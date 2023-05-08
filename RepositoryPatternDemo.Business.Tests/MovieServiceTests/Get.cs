69ace501fb5882d6f177bb1f7c7c9d1ac8cf42f4
kenji.elzerman <kenji.elzerman@gmail.com>
2023-05-08 10:08:43 +0200

    Adding unit test

---
+++ RepositoryPatternDemo.Business.Tests/MovieServiceTests/Get.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using RepositoryPatternDemo.Business.Repositories;
+using RepositoryPatternDemo.Domain.Entities;
+
+namespace RepositoryPatternDemo.Business.Tests.MovieServiceTests
+{
+    public class Get
+    {
+        [Fact]
+        public void Should_ReturnFiveMovies()
+        {
+            Mock<IGenreRepository> mockGenreRepository = new();
+            Mock<IMovieRepository> mockMovieRepository = new();
+
+            mockMovieRepository.Setup(x => x.Get()).Returns(new List<Movie>
+            {
+                new Movie { Id = 1 },
+                new Movie { Id = 2 },
+                new Movie { Id = 3 },
+                new Movie { Id = 4 },
+                new Movie { Id = 5 },
+            }.AsQueryable);
+
+            MovieService classUnderTest = new(mockMovieRepository.Object, mockGenreRepository.Object);
+
+            List<Movie> result = classUnderTest.Get().ToList();
+
+            Assert.Equal(5, result.Count);
+        }
+    }
+}
